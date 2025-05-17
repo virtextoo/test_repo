bda2e714ff45ffb1031070052e7f70e86c63862e
virtextoo <77986058+virtextoo@users.noreply.github.com>
2025-05-16 23:19:50 -0700

    Add 4-bit counter implementation

--- counter.sv
+++ counter.sv
@@ -1,1 +1,15 @@
-test
+module counter (
+    input logic clk,
+    input logic reset,
+    output logic [3:0] count
+);
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            count <= 4'b0000;
+        end else begin
+            count <= count + 1'b1;
+        end
+    end
+
+endmodule
